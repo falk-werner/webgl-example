d2b40a5804f93ea0a60808a8cd2cbd0648da2b14
Falk Werner <falk.werner@gmx.net>
2021-06-13 15:56:44 +0200

    draw cube

--- js/fragment_shader.js
+++ js/fragment_shader.js
@@ -2,11 +2,15 @@ const g_fragment_shader = `#version 300 es
 
 precision highp float;
 
+in vec2 UV;
 out vec4 color;
+
+uniform sampler2D myTextureSampler;
 
 void main()
 {
-    color = vec4(1, 0, 0, 1);
+    color = texture(myTextureSampler, UV);
+    // color = vec4(1, 0, 0, 1);
 }
 
 `;
--- js/startup.js
+++ js/startup.js
@@ -41,9 +41,53 @@ function startup() {
     const mvp = mat4_mul(mat4_mul(view, projection), model);
 
     const vertex_buffer_data = [
-        -1.0, -1.0, 0.0,
-         1.0, -1.0, 0.0,
-         0.0,  1.0, 0.0
+        -1.0, -1.0, -1.0,
+        -1.0, -1.0, 1.0,
+        -1.0,  1.0, 1.0,
+
+         1.0,  1.0, -1.0,
+        -1.0, -1.0,  1.0,
+        -1.0,  1.0, -1.0,
+
+         1.0,-1.0, 1.0,
+        -1.0,-1.0,-1.0,
+         1.0,-1.0,-1.0,
+        
+         1.0, 1.0,-1.0,
+         1.0,-1.0,-1.0,
+        -1.0,-1.0,-1.0,
+    
+        -1.0,-1.0,-1.0,
+        -1.0, 1.0, 1.0,
+        -1.0, 1.0,-1.0,
+    
+         1.0,-1.0, 1.0,
+        -1.0,-1.0, 1.0,
+        -1.0,-1.0,-1.0,
+    
+        -1.0, 1.0, 1.0,
+        -1.0,-1.0, 1.0,
+         1.0,-1.0, 1.0,
+    
+         1.0, 1.0, 1.0,
+         1.0,-1.0,-1.0,
+         1.0, 1.0,-1.0,
+    
+         1.0,-1.0,-1.0,
+         1.0, 1.0, 1.0,
+         1.0,-1.0, 1.0,
+    
+         1.0, 1.0, 1.0,
+         1.0, 1.0,-1.0,
+        -1.0, 1.0,-1.0,
+    
+         1.0, 1.0, 1.0,
+        -1.0, 1.0,-1.0,
+        -1.0, 1.0, 1.0,
+    
+         1.0, 1.0, 1.0,
+        -1.0, 1.0, 1.0,
+         1.0,-1.0, 1.0
     ];
 
     const vertex_buffer = gl.createBuffer();
@@ -62,13 +106,96 @@ function startup() {
         0,
         0);
 
+    const texture = gl.createTexture();
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    const cube_image = document.querySelector('#cube');
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, cube_image);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_LINEAR);
+    gl.generateMipmap(gl.TEXTURE_2D);
+
+    const texture_id = gl.getUniformLocation(program, "myTextureSampler");
+
+    const a = 0.325;
+    const uv_buffer_data = [
+        0 * a, (0 * a),
+        0 * a, (1 * a),
+        1 * a, (1 * a),
+
+        3 * a, (0 * a),
+        2 * a, (1 * a),
+        3 * a, (1 * a),
+
+        2 * a, (1 * a),
+        1 * a, (2 * a),
+        2 * a, (2 * a),
+
+        3 * a, (0 *a),
+        2 * a, (0 * a),
+        2 * a, (1 * a),
+
+        0 * a, (0 * a),
+        1 * a, (1 *a),
+        1 * a, (0 * a),
+
+        2 * a, (1 * a),
+        1 * a, (1* a),
+        1 * a, (2*a),
+
+        3 * a, (2 * a),
+        3 * a, (1 * a),
+        2 * a, (1 * a),
+
+        2 * a, (0 * a),
+        1 * a, (1 * a),
+        2 * a, (1 * a),
+
+        1 * a, (1 * a),
+        2 * a, (0 * a),
+        1 * a, (0 * a),
+
+        0 * a, (1 * a),
+        0 * a, (2 * a),
+        1 * a, (2 * a),
+
+        0 * a, (1 * a),
+        1 * a, (2 * a),
+        1 * a, (1 * a),
+
+        2 * a, (2 * a),
+        3 * a, (2 * a),
+        2 * a, (1 * a)
+    ];   
+
+    const uv_buffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, uv_buffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(uv_buffer_data), gl.STATIC_DRAW);
+
+    gl.enable(gl.DEPTH_TEST);
+    gl.enable(gl.CULL_FACE);
+
+    // ----
 
     gl.clear(gl.COLOR_BUFFER_BIT);
     gl.useProgram(program);
 
     gl.uniformMatrix4fv(matrix, false, new Float32Array(mvp));
 
-    gl.drawArrays(gl.TRIANGLES, 0, 3);
+    gl.activeTexture(gl.TEXTURE0);
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    //gl.uniformli(texture_id, 0);
+
+    gl.enableVertexAttribArray(1);
+    gl.bindBuffer(gl.ARRAY_BUFFER, uv_buffer);
+    gl.vertexAttribPointer(
+        1,
+        2,
+        gl.FLOAT,
+        gl.FLOAT,
+        0,
+        0);
+
+    gl.drawArrays(gl.TRIANGLES, 0, vertex_buffer_data.length / 3);
     gl.flush();
 
 }
